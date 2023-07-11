0c8069c1c47aac942e43a168252ce7a03bcdff97
Mayco Barale <maycobarale@gmail.com>
2023-07-11 17:30:01 -0300

    Update accomodation page

--- app/donde-dormir/[slug]/page.tsx
+++ app/donde-dormir/[slug]/page.tsx
@@ -21,22 +21,22 @@ export default async function Page({ params }: Props) {
   return (
     <>
       <Hero title={name} imageSrc={image} imageAlt={name} />
-      <div className="max-w-screen-2xl mx-auto">
-        <section className="text-lg max-w-prose mx-auto py-12">
+      <div className="max-w-screen-2xl mx-auto px-8">
+        <section className="text-lg max-w-prose mx-auto py-12 px-8">
           {/* TODO: aceptar contenido HTML aca para poder formatear el contenido, una idea puede ser aceptar dos columenas */}
           {description}
         </section>
-        <section className="bg-green p-12 rounded-lg">
+        <section className="bg-green py-12 px-8 rounded-lg">
           <h2 className="uppercase text-2xl font-bold text-white">Datos de contacto</h2>
           <div className="grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-8 pt-6">
             <ContactCell info={contactInfo} />
           </div>
         </section>
-        <section className="p-12">
+        <section className="py-12 px-8">
           <h2 className="uppercase text-2xl font-bold text-green">Ubicacion</h2>
           <iframe className="pt-8" src={mapURL} width="100%" height="450" style={{ 'border': 0 }} loading="lazy"></iframe>
         </section>
-        <section className="p-12">
+        <section className="p-12 px-8">
           <h2 className="uppercase text-2xl font-bold text-green">Galería de imágenes</h2>
           {/* TODO: hace galeria de imagenes */}
         </section>
